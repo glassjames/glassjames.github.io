6968dc1d98ea79199124406e00f55bd9ffaa9f0f
James Glass <james.glass14@gmail.com>
2018-01-27 15:43:22 -0600

    Moved packery files. Building new site

--- packNdrag.js
+++
@@ -1,8 +0,0 @@
-var pckry = new Packery( '.grid', {
-  itemSelector: '.tile',
-});
-
-pckry.getItemElements().forEach( function( itemElem ) {
-  var draggie = new Draggabilly( itemElem );
-  pckry.bindDraggabillyEvents( draggie );
-});
