bad24f92daa11d1983c1257bd57c0638fa78464b
James Glass <james.glass14@gmail.com>
2018-01-31 14:20:55 -0600

    Added hover func to name and links

--- initial.js
+++ initial.js
@@ -2,4 +2,52 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['home', 'about', 'work', 'resume', 'contact']
   });
+  $(".hvr-pop").hover(function(){
+        var num = Math.floor(Math.random() * 4);
+        switch(num) {
+          case 0:
+            var color = "#33C55A";
+          break;
+          case 1:
+            var color = "#1476E3";
+          break;
+          case 2:
+            var color = "#DD155D";
+          break;
+          case 3:
+            var color = "#F4D46C";
+          break;
+        }
+        $(this).css("color", color);
+        }, function(){
+        $(this).css("color", "#FFFFFF");
+  });
+  $("#about-link").hover(function(){
+        $(this).css("color", "#FFFFFF");
+        }, function(){
+        $(this).css("color", "#33C55A");
+  });
+  $("#work-link").hover(function(){
+        $(this).css("color", "#FFFFFF");
+        }, function(){
+        $(this).css("color", "#1476E3");
+  });
+  $("#resume-link").hover(function(){
+        $(this).css("color", "#FFFFFF");
+        }, function(){
+        $(this).css("color", "#DD155D");
+  });
+  $("#contact-link").hover(function(){
+        $(this).css("color", "#FFFFFF");
+        }, function(){
+        $(this).css("color", "#F4D46C");
+  });
 });
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
