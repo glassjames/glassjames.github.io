173dd08573ff80179527c924ea2a9a09fe0dfa5a
James Glass <james.glass14@gmail.com>
2018-02-08 13:31:56 -0600

    Added more content to work page

--- initial.js
+++ initial.js
@@ -1,6 +1,41 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['home', 'about', 'work', 'resume', 'contact']
+    anchors: ['home', 'about', 'work', 'resume', 'contact'],
+    menu: '#menu',
+    scrollBar:true,
+    css3: true,
+		slidesNavigation: true,
+		slidesToSections: true,
+		responsiveWidth: 900,
+		responsiveSlides: true,
+		responsiveSlidesKey: 'YWx2YXJvdHJpZ28uY29tX3RoVWNtVnpjRzl1YzJsMlpWTnNhV1JsY3c9PUVZdg==',
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+			console.log("slideLoad--" + "anchorLink: " + anchorLink + " index: " + index + " slideAnchor: " + slideAnchor + " slideIndex: " + slideIndex);
+
+		},
+		onSlideLeave: function(anchorLink, index, slideIndex, direction){
+			console.log("----------------");
+			console.log("onSlideLeave--" + "anchorLink: " + anchorLink + " index: " + index + " slideIndex: " + slideIndex + " direction: " + direction);
+		},
+    afterRender: function(){
+      $('.owl-carousel').owlCarousel({
+        stagePadding: 75,
+        loop:true,
+        margin: 0,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+      })
+	  }
   });
   $(".hvr-pop").hover(function(){
         var num = Math.floor(Math.random() * 4);
@@ -44,3 +79,12 @@ $(document).ready(function() {
   });
   $.fn.fullpage.reBuild();
 });
+
+function toggleProjects() {
+  if ($('.check').is(':checked')) {
+    $('.item').css('background-color','#FFFFFF');
+  }
+  else {
+    $('.item').css('background-color','#1476E3');
+  }
+}
